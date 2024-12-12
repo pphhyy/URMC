7cff1f75e8bcc054bb1c8f85599b38002658ef39
nimragiscoming <nine.seven.ninete@gmail.com>
2024-12-12 04:41:19 +0000

    added mech explosion comp to tartarus mk o1

---
+++ Source/URMC/CompMechDetonator.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+using RimWorld;
+using Verse.Sound;
+using System.Collections.Generic;
+using Verse.AI.Group;
+using System;
+using UnityEngine;
+
+namespace URMC
+{
+    public class CompMechDetonator : ThingComp
+    {
+        public CompProperties_MechDetonator Props => (CompProperties_MechDetonator)props;
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+
+            if (parent.Faction == Faction.OfPlayer)
+            {
+                Command_Action command_Action = new Command_Action();
+                command_Action.icon = Props.GetUiIcon();
+                command_Action.defaultLabel = "URMC_MechDetonate".Translate();
+                command_Action.action = delegate
+                {
+                    //Log.Message("BOOM");
+                    parent.Kill();
+                };
+                yield return command_Action;
+            }
+
+        }
+    }
+}
